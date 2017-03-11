59060844fd63b4a9c463f6cb5ad66bb9d6655a18
Colton McInroy <colton@dosarrest.com>
2017-03-10 18:14:08 -0800

    Fixed inittab, nano being removed from depclean, and depclean

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ MAINTAINER DOSarrest IT Team
 # This one should be present by running the build.sh script
 ADD install.sh /
 
-RUN sed -e 's/USE="/USE="vim-syntax -xattr /g' -i /etc/portage/make.conf
+RUN sed -e 's/USE="/USE="vim-syntax -xattr -seccomp /g' -i /etc/portage/make.conf
+
+RUN sed -e 's/^c/#c/g' -i /etc/inittab
 
 RUN /install.sh
--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
-# Say hello
-#echo "Bootstrapped ${stage} into /:"
-ls --color -lah
+echo " ---> Running Emerge Web RSync"
 emerge-webrsync
+echo " ---> Installing portage"
 emerge -1 -j2 -v portage
+echo " ---> Installing nano"
+emerge -j2 -v nano
+echo " ---> Updating world"
 emerge --exclude="glibc" -j2 -vuND @world
+echo " ---> Removing unnecessary sysinit tasks"
+rc-update del udev sysinit
+rc-update del udev-trigger sysinit
+rc-update del kmod-static-nodes sysinit
+rc-update del opentmpfiles-dev sysinit
+echo " ---> Cleaning up"
+emerge --depclean
 rm -fr /usr/portage/*
-
+rm install.sh
